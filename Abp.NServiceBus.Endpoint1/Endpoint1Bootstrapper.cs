a91591da0ee9e98ace98c950ba462dfc046da394
Bruno Bertechini <bruno.bertechini@outlook.com>
2016-11-18 10:57:55 -0200

    Configure endpoint to rely on Abp.NServiceBus to start endpoint

--- Abp.NServiceBus.Endpoint1/Endpoint1Bootstrapper.cs
+++ Abp.NServiceBus.Endpoint1/Endpoint1Bootstrapper.cs
@@ -3,6 +3,7 @@ using Abp.Modules;
 using Abp.Runtime.Session;
 using Castle.MicroKernel.Registration;
 using NServiceBus;
+using NServiceBus.Logging;
 using NServiceBus.Persistence;
 using NServiceBus.Transport.SQLServer;
 using System;
@@ -25,85 +26,21 @@ namespace Abp.NServiceBus
     {
         public const string EndpointName = "Abp.NServiceBus.Endpoint1";
 
+        public override void PreInitialize()
+        {
+            var defaultConnString = ConfigurationManager.ConnectionStrings[NServiceBusConsts.ConnectionStringName].ConnectionString;
+
+            Configuration.Modules.NServiceBus().Debug = true;
+            Configuration.Modules.NServiceBus().EndpointName = EndpointName;
+            Configuration.Modules.NServiceBus().LogDirectory = ConfigurationManager.AppSettings["LogDirPath"];
+            Configuration.Modules.NServiceBus().TransportConnectionString = defaultConnString;
+            Configuration.Modules.NServiceBus().PersistenceConnectionString = defaultConnString;
+            Configuration.Modules.NServiceBus().LogLevel = LogLevel.Info;
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
-
-            var endpointConfiguration = new EndpointConfiguration(Endpoint1Bootstrapper.EndpointName);
-            endpointConfiguration.PurgeOnStartup(false);
-            endpointConfiguration.SendFailedMessagesTo("error");
-            endpointConfiguration.AuditProcessedMessagesTo("audit");
-
-            // Default Connection String
-            var defaultConnString = ConfigurationManager.ConnectionStrings[NServiceBusConsts.ConnectionStringName].ConnectionString;
-
-            // SQL Server Transport - (Package NServiceBus.SqlServer)
-            var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
-            transport.ConnectionString(defaultConnString);
-            transport.DefaultSchema(NServiceBusConsts.EndpointDatabaseSchema);
-
-            // NHibernate Persistence - (Package NServiceBus.NHibernate)
-            var nhConfiguration = new NHibernate.Cfg.Configuration
-            {
-                Properties =
-                {
-                    ["dialect"] = "NHibernate.Dialect.MsSql2012Dialect",
-                    ["connection.provider"] = "NHibernate.Connection.DriverConnectionProvider",
-                    ["connection.driver_class"] = "NHibernate.Driver.SqlClientDriver",
-                    ["connection.default_schema"] = NServiceBusConsts.EndpointDatabaseSchema,
-                    ["connection.connection_string"] = defaultConnString
-                }
-            };
-            var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
-            persistence.UseConfiguration(nhConfiguration);
-
-            // Unobtrusive Message Mode
-            endpointConfiguration.UseAbpNServiceBusMessageConventions();
-
-            // Disable Immediate & Delayed Retries 
-            endpointConfiguration.Recoverability()
-                .Immediate(customizations: immediate =>
-                {
-                    immediate.NumberOfRetries(0);
-                })
-                .Delayed(customizations: delayed =>
-                {
-                    delayed.NumberOfRetries(3);
-                    delayed.TimeIncrease(TimeSpan.FromSeconds(30));
-                });
-
-            // Configure NServiceBus with Abp IocContainer - (Package NServiceBus.CastleWindsor)
-            endpointConfiguration.UseContainer<WindsorBuilder>(
-                customizations: customizations =>
-                {
-                    customizations.ExistingContainer(IocManager.IocContainer);
-                });
-
-            // Configure Behavior for AbpNServiceBusSession
-            endpointConfiguration.Pipeline.Register(typeof(AbpNServiceBusUnitOfWork), typeof(AbpNServiceBusUnitOfWork).Name);
-
-            // Enable Installers & Start Endpoint
-            endpointConfiguration.EnableInstallers();
-            var endpointInstance = Endpoint.Start(endpointConfiguration)
-                                           .GetAwaiter()
-                                           .GetResult();
-
-            // Register Endpoint Instance to Abp Container
-            IocManager.IocContainer.Register(
-                Component.For<IEndpointInstance>().Instance(endpointInstance)
-            );
-
-            Console.WriteLine(string.Format("NServiceBus endpoint {0} started successfully", Endpoint1Bootstrapper.EndpointName));
-        }
-
-        public override void Shutdown()
-        {
-            IEndpointInstance endpoint = IocManager.Resolve<IEndpointInstance>();
-            endpoint.Stop()
-                    .GetAwaiter()
-                    .GetResult();
-
-            Console.WriteLine(string.Format("NServiceBus endpoint {0} shutdown successfully", EndpointName));
         }
     }
 }
